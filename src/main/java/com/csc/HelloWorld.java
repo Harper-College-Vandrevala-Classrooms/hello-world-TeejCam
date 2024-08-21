8b5843176952db3ac4304cc5dd87477f944980a2
TeejCam <tjcamalan@gmail.com>
2024-08-21 11:22:11 -0500

    Added a hello world message.

--- src/main/java/com/csc/HelloWorld.java
+++ src/main/java/com/csc/HelloWorld.java
@@ -1,3 +1,10 @@
 package com.csc;
 
 // Your code goes here!
+package com.csc;
+
+public class HelloWorld (
+    public static void main(String[] args) {
+        System.out.print("Hello World!\n");
+    }
+)
